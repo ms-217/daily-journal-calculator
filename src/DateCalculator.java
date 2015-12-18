e6b6c908c2e0039a7e8b95017ee043e982e87da7
Mason Schmidgall <madogstony101@gmail.com>
2015-12-17 22:46:00 -0600

    Add startDate and endDate instance variables to DateCalculator
    Added startDate and endDate with getters and setters.

--- src/DateCalculator.java
+++ src/DateCalculator.java
@@ -1,4 +1,5 @@
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Mason Schmidgall on 12/17/2015.
@@ -6,10 +7,27 @@ import java.util.Calendar;
 public class DateCalculator {
 
     private Calendar calendar;  //calendar for calculating date
+    private Date startDate;
+    private Date endDate;
 
     public DateCalculator() {
         calendar = Calendar.getInstance();
         
     }
 
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
