d6b3dffa2b2d1ed7088baf384924bf5a9071ddb7
Mason Schmidgall <madogstony101@gmail.com>
2015-12-17 22:36:50 -0600

    Add DateCalculator Class with Constructor
    Constructor not completed. Also added an empty constructor to
    JournalCalculator.

---
+++ src/DateCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+
+/**
+ * Created by Mason Schmidgall on 12/17/2015.
+ */
+public class DateCalculator {
+
+    private Calendar calendar;  //calendar for calculating date
+
+    public DateCalculator() {
+        calendar = Calendar.getInstance();
+        
+    }
+
+}
--- src/JournalCalculator.java
+++ src/JournalCalculator.java
@@ -1,5 +1,11 @@
 /**
  * Created by Mason Schmidgall on 12/17/2015.
  */
+
 public class JournalCalculator {
+
+
+    public JournalCalculator() {
+
+    }
 }
